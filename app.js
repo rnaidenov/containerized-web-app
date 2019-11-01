5a8e5d3dceed80fb9b7e35649f70415a0a90507b
Radoslav Naydenov <radoslav.naydenov@publicissapient.com>
2019-11-01 05:22:59 +0000

    Added client code of the application / need to include responsiveness

---
+++ app.js
@@ -0,0 +1,64 @@
+const API_BASE_URL = 'http://localhost:3000/grads';
+const searchBtn = document.querySelector('.searchBtn');
+const inputFields = Array.from(document.querySelectorAll('.textFieldWrap > input'));
+const explosionAudio = document.querySelector('.boomAudio');
+const gradsResults = document.querySelector('.data');
+const gradDataBox = document.querySelector('.gradDataBox');
+
+
+searchBtn.addEventListener('click', async (e) => {
+    // Prevent page from refreshing
+    e.preventDefault();
+    clearData();
+    const gradsData = await fetchGradsData();
+    showGradsData(gradsData);
+    // explosionAudio.play();
+    addExplosion();
+    console.log({ gradsData });
+});
+
+
+// Fetching grads data from API
+const fetchGradsData = () => {
+    const inputValues = inputFields.map(field => [field.name, field.value]);
+
+    const queryParams = inputValues.reduce((acc,val) => {
+        const [param, value] = val;
+
+        if(!value) return acc;
+
+        const paramValuePair = `${param}=${value}`;
+        return acc += acc ? `&${paramValuePair}` : `?${paramValuePair}`
+    }, '');
+    
+    return fetch(`${API_BASE_URL}${queryParams}`).then(res => res.json());
+}
+
+
+
+const addExplosion = () => {
+    gradDataBox.innerHTML += `
+                                <div class="explosion">
+                                    <img src="/assets/shockwave.svg" class='explosion--image' alt="BOOM!">
+                                </div>
+                            `;
+
+
+    setTimeout(() => { 
+        gradDataBox.lastChild.remove();
+    }, 4000);
+}
+
+
+const showGradsData = (grads) => {
+    const filteredGrads = grads.map(grad => grad.firstName).join(', '); 
+    gradsResults.textContent += filteredGrads;
+    gradsResults.classList.add('generated');
+}
+
+
+const clearData = () => {
+    inputFields.forEach(field => field.value = '');
+    gradsResults.textContent = '';
+    gradsResults.classList.remove('generated');    
+}
